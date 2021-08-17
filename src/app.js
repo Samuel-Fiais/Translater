d29ade699d9d0ebfa89437ad4acfbe7072a0e6d2
Samuel Fiais <samuel.fiaissf1@gmail.com>
2021-08-17 12:01:48 -0300

    update

--- src/app.js
+++ src/app.js
@@ -7,25 +7,25 @@ const readlineSync = require('readline-sync');
     const page = await browser.newPage();
     await page.goto('https://www.google.com/search?q=tradutor');
 
-    const idiomaBase = readlineSync.question("Idioma: ") || "Português";
-    const texto = readlineSync.question("Texto: ") || "Hello world";
-    const idiomaFinal = readlineSync.question("Idioma para Traduzir: ") || "Inglês";
+    const base_language = readlineSync.question("Language: ") || "Português";
+    const text = readlineSync.question("Text: ") || "Hello world";
+    const final_language = readlineSync.question("Language to Translate: ") || "Inglês";
 
     await page.click('[class="source-language"]');
-    await page.type('[class="tw-lp-search hide-focus-ring"]', idiomaBase);
+    await page.type('[class="tw-lp-search F0azHf"]', base_language);
     await page.keyboard.press(String.fromCharCode(13));
 
     await page.click('[class="target-language"]');
-    await page.type('[class="tw-lp-search hide-focus-ring"]', idiomaFinal);
+    await page.type('[class="tw-lp-search F0azHf"]', final_language);
     await page.keyboard.press(String.fromCharCode(13));
     
-    await page.type('[class="tw-ta tw-text-large XcVN5d goog-textarea"]', texto);
+    await page.type('[class="tw-ta tw-text-large XcVN5d goog-textarea"]', text);
     
     await page.waitForTimeout(2000);
-    const textoTraduzido = await page.evaluate(() => {
-        return document.querySelector('#tw-target-text > span').textContent;
+    const translated_text = await page.evaluate(() => {
+        return document.getElementsByClassName('Y2IQFc')[2].textContent;
     });
-    console.log(`O texto traduzido de ${idiomaBase} para ${idiomaFinal} é:\n`);
-    console.log(textoTraduzido);
+    console.log(`\nTranslation from ${base_language} to ${final_language} is:\n`);
+    console.log(translated_text);
     await browser.close();
 })();
